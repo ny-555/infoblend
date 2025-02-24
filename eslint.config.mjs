14fc10c04802e298c4ab52d9b0db3695bded2a4d
ny-555 <nywork.fb1234@gmail.com>
2025-02-24 16:20:37 +0900

    feat:ブログページの追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["next.config.ts", "tailwind.config.ts"],
+    rules: {
+      "@typescript-eslint/no-require-imports": "off", // next.config.tsのみrequire()を許可
+    },
+  },
 ];
 
 export default eslintConfig;
