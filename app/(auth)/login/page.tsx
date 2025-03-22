ab87fdab5d24ff742ed4a01523eb99051ca57bfe
ny-555 <nywork.fb1234@gmail.com>
2025-03-22 19:02:08 +0900

    style:ログイン画面のスタイル修正

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,7 +5,7 @@ export default function Login() {
     <div className="container mx-auto px-8 flex flex-col justify-center h-screen items-center w-screen">
       <div className="w-full sm:w-[450px] flex flex-col space-y-6">
         <div className="md:text-center space-y-4">
-          <h1 className="text-4xl md:5xl lg:text-6xl font-bold">
+          <h1 className="text-4xl md:5xl font-bold">
             ようこそ。または、
             <br />
             おかえりなさい。
